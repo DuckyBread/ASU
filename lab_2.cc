8895507788fcb7265c9edb250345e421ac6694e4
Silvia Julieann <silviaju@si-a6.nsk.21-school.ru>
2023-03-30 22:37:35 +0700

    функция --> метод, исправлено + на *

--- lab_2.cc
+++ lab_2.cc
@@ -4,11 +4,12 @@
 
 class Integrator {
  public:
-  Integrator() : Integrator(0, 1, 0, 0) {}
+  Integrator() = default;
   Integrator(float newValue, float newScale, float newMin, float newMax)
       : value_(newValue), scale_(newScale), MIN_(newMin), MAX_(newMax) {}
   ~Integrator() {}
-  Integrator(const Integrator&) {}
+  Integrator(const Integrator& i)
+      : value_(i.value_), scale_(i.scale_), MIN_(i.MIN_), MAX_(i.MAX_) {}
   Integrator(Integrator&&) = delete;
 
   void SetValue(float value) { value_ = value; }
@@ -21,6 +22,23 @@ class Integrator {
   float GetMin() { return MIN_; }
   float GetMax() { return MAX_; }
 
+  std::ofstream ClassIntegrator(float time_start, float time_end, float dt) {
+    std::string file_name;
+    std::cout << "Enter file name:\n";
+    std::cin >> file_name;
+    file_name += ".txt";
+
+    std::ofstream FS(file_name);
+
+    for (auto t = time_start; t <= time_end; t += dt) {
+      float u = (t < 0) ? (0.) : (1.);
+      SetValue(GetValue() + GetScale() * u);
+      FS << t << "\t" << u << "\t" << GetValue() << std::endl;
+    }
+
+    return FS;
+  }
+
   void ResetIntegrator() {
     value_ = MIN_ = MAX_ = 0;
     scale_ = 1;
@@ -31,25 +49,22 @@ class Integrator {
   }
 
  private:
-  float value_, scale_, MIN_, MAX_;
+  float value_ = 0, scale_ = 1, MIN_ = 0, MAX_ = 0;
 };
 
 std::ofstream integrator(float time_start, float time_end, float dt,
                          float gain);
-std::ofstream ClassIntegrator(Integrator& param, float time_start,
-                              float time_end, float dt);
 
 int main() {
   float t0 = -1.;   /* Start time */
   float tmax = 10.; /* Finish time */
   float dt = 0.001; /* Step = 0.01 s */
   // float gain = 1.5;
-
   Integrator aboba(0, 1.5f, 0, 0);
 
   std::cout << "Start\n";
   // std::ofstream FS = integrator(t0, tmax, dt, gain);
-  std::ofstream FS = ClassIntegrator(aboba, t0, tmax, dt);
+  std::ofstream FS = aboba.ClassIntegrator(t0, tmax, dt);
   aboba.PrintIntegrator();
   std::cout << "end of the prog\n";
 
@@ -71,23 +86,5 @@ std::ofstream integrator(float time_start, float time_end, float dt,
     intgr += gain * u;
     FS << t << "\t" << u << "\t" << intgr << std::endl;
   }
-  return FS;
-}
-
-std::ofstream ClassIntegrator(Integrator& param, float time_start,
-                              float time_end, float dt) {
-  std::string file_name;
-  std::cout << "Enter file name:\n";
-  std::cin >> file_name;
-  file_name += ".txt";
-
-  std::ofstream FS(file_name);
-
-  for (auto t = time_start; t <= time_end; t += dt) {
-    float u = (t < 0) ? (0.) : (1.);
-    param.SetValue(param.GetValue() + param.GetScale() + u);
-    FS << t << "\t" << u << "\t" << param.GetValue() << std::endl;
-  }
-
   return FS;
 }
